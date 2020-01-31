ba888dcecf1c5b9d641156e227dbc80218797aab
Darrel Scott <darrel.scott26@gmail.com>
2020-01-31 17:47:24 -0500

    Clean and add safety to datahandler.py

--- pricebot/datahandler.py
+++ pricebot/datahandler.py
@@ -3,6 +3,9 @@ import json
 
 #TODO: Write tests for format_data ASAP
 
+class InvalidPart(Exception):
+    pass
+
 class DataHandler:
     def __init__(self):
         self.api = API()
@@ -18,11 +21,10 @@ class DataHandler:
         """Convert self.raw_data into json, get the data we need, and return a list with the data on various parts"""
 
         json_data = [None, None, None, None, None, None, None]
-        for i in range(0,len(self.raw_data)):
-            #A stupidly long oneliner to remove the unecessary object at the start of the json
-            json_data[i] = json.loads(self.raw_data[i].to_json())[next(iter(json.loads(self.raw_data[i].to_json())))]
+        for i in range(len(self.raw_data)):
+            top_level_key = next(self.raw_data[i].keys())
+            json_data[i] = json.loads(self.raw_data[i].to_json())[top_level_key]
 
-        #A list of dictionaries of a list of dictionaries
         formatted_data = {"cpu": [], 
                           "gpu": [], 
                           "psu": [], 
@@ -31,14 +33,18 @@ class DataHandler:
                           "hdd": [],
                           "case": []}
 
+        key = next(formatted_data).key()
         for i in range(0,len(json_data)):
-            formatted_data[next(iter(formatted_data))].append({
-                                                                     (str(json_data[i]["brand"]) + " " + str(json_data[i]["model"])): 
-                                                                      "$" + str(json_data[i]["price"][1])
-                                                                   })
+            formatted_data[key].append({
+                                        f"{json_data[i]['brand']} {json_data[i]['model']}": 
+                                        f"${json_data[i]['price'][1]}"
+                                      })
         return formatted_data
 
     def get_part_data(self, part):
         """Retrieve the data on a particular part out of the mess that is the return of format_data
            Parts must be cpu, gpu, psu, ram, mobo, hdd, or case"""
-        return self.format_data()[part]
+        try:
+            return self.format_data()[part]
+        except KeyError:
+            raise InvalidPart("User entered a part that isn\'t supported/doesn\'t exist")
